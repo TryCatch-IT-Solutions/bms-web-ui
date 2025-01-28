b13fbd71e82759b37feae7a4e89f9295190528a2
Cyrus Belanio <cyrus.belanio@hoolisoftware.com>
2025-01-28 14:17:13 +0800

    BMS-248 fixed same phone and em contact number in my profile

--- src/pages/private/Users/UserProfile/ProfileForm/index.tsx
+++ src/pages/private/Users/UserProfile/ProfileForm/index.tsx
@@ -54,6 +54,7 @@ export const ProfileForm: React.FC = () => {
 
     const {
         setValue,
+        setError,
         formState: { errors, isValid, isDirty },
     } = userForm
 
@@ -70,7 +71,11 @@ export const ProfileForm: React.FC = () => {
     })
 
     const onSubmit = (data: EditUserType) => {
-        updateUserMu(data)
+        if (data.phone_number === data.emergency_contact_no) {
+            setError('emergency_contact_no', { message: 'Cannot be the same as user phone number' })
+        } else {
+            updateUserMu(data)
+        }
     }
 
     useEffect(() => {
@@ -477,7 +482,7 @@ export const ProfileForm: React.FC = () => {
                                                             />
                                                         </FormControl>
                                                         <FormMessage>
-                                                            {errors?.phone_number?.message}
+                                                            {errors?.emergency_contact_no?.message}
                                                         </FormMessage>
                                                     </FormItem>
                                                 )}
