6337318936364c2e62bcce8cc5844b25f71f145b
Cyrus Belanio <cyrus.belanio@hoolisoftware.com>
2025-02-03 10:57:41 +0800

    BMS-288 add password overide for admins

--- src/pages/private/Users/UserProfile/index.tsx
+++ src/pages/private/Users/UserProfile/index.tsx
@@ -1,13 +1,18 @@
-import { useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
 import { ProfileForm } from './ProfileForm'
 import { userProfileTabsAtom } from '@/store/user'
 import { UserProfileTabs } from './UserProfileTabs'
 import { BreadCrumbs } from '@/components/BreadCrumbs'
 import { USER_PROFILE_TABS } from '@/constants'
 import { PasswordForm } from './PasswordForm'
+import { useEffect } from 'react'
 
 export const UserProfile = () => {
-    const selected = useAtomValue(userProfileTabsAtom)
+    const [selected, setSelected] = useAtom(userProfileTabsAtom)
+
+    useEffect(() => {
+        setSelected(USER_PROFILE_TABS.PROFILE)
+    }, [])
 
     return (
         <div className='content'>
