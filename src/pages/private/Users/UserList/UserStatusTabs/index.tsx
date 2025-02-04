ddbf680d0d9d62b7eebdcd7d69d474fdc1531130
Cyrus Belanio <cyrus.belanio@hoolisoftware.com>
2025-02-04 10:58:52 +0800

    BMS-292 separate table for assigned and unassigned users

--- src/pages/private/Users/UserList/UserStatusTabs/index.tsx
+++ src/pages/private/Users/UserList/UserStatusTabs/index.tsx
@@ -1,9 +1,15 @@
 import { getUserStatusCount } from '@/api/profile'
-import { USER_STATUS } from '@/constants'
-import { userIdsToDeleteAtom, userSelectedStatusAtom, usersToExportAtom } from '@/store/user'
+import { EMP_ASSIGN_STATUS_TABS, USER_ASSIGN_STATUS_TABS, USER_STATUS } from '@/constants'
+import {
+    userAssignStatusFilterAtom,
+    userIdsToDeleteAtom,
+    userSelectedStatusAtom,
+    usersToExportAtom,
+} from '@/store/user'
 import { Tabs, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { useSetAtom } from 'jotai'
+import { useEffect } from 'react'
 
 interface UserTabsProps {
     search?: string
@@ -16,45 +22,80 @@ export const UserStatusTabs: React.FC<UserTabsProps> = ({ search, roles, availab
     const setSelectedStatus = useSetAtom(userSelectedStatusAtom)
     const setToDelete = useSetAtom(userIdsToDeleteAtom)
     const setToExport = useSetAtom(usersToExportAtom)
+    const setuserAssignStatusFilterAtom = useSetAtom(userAssignStatusFilterAtom)
 
     const queryClient = useQueryClient()
 
     const onSwitchTab = (status: string) => {
         queryClient.invalidateQueries({ queryKey: ['usersList'] })
-        setSelectedStatus(status)
+        const isAssigned =
+            status === EMP_ASSIGN_STATUS_TABS?.unassigned ||
+            status === EMP_ASSIGN_STATUS_TABS?.archive
+
+        const isActive =
+            status === EMP_ASSIGN_STATUS_TABS?.unassigned ||
+            status === EMP_ASSIGN_STATUS_TABS?.assigned
+
+        setuserAssignStatusFilterAtom(isAssigned)
+
+        setSelectedStatus(isActive ? USER_STATUS.ACTIVATED : USER_STATUS.INACTIVE)
         setToDelete(null)
         setToExport(null)
     }
 
-    const { data: userCount } = useQuery({
-        queryKey: ['userStatusCount', search, roles, available],
-        queryFn: () => getUserStatusCount('users', search, roles, available),
+    const { data: assignedCount } = useQuery({
+        queryKey: ['assignedUsersCount', search, roles, available],
+        queryFn: () => getUserStatusCount('users', search, roles, false),
     })
+
+    const { data: unassignedCount } = useQuery({
+        queryKey: ['unassignedUsersCount', search, roles, available],
+        queryFn: () => getUserStatusCount('users', search, roles, true),
+    })
+
+    const { data: archivedCount } = useQuery({
+        queryKey: ['archivedUsersCount', search, roles, available],
+        queryFn: () => getUserStatusCount('users', search, roles, true),
+    })
+
+    useEffect(() => {
+        setSelectedStatus(USER_STATUS.ACTIVATED)
+        setuserAssignStatusFilterAtom(false)
+    }, [])
 
     return (
         <Tabs
-            defaultValue={USER_STATUS.ACTIVATED}
+            defaultValue={USER_ASSIGN_STATUS_TABS.assigned}
             onValueChange={(val) => setSelectedStatus(val)}
             className='bg-white rounded-md w-full'
         >
             <TabsList className='w-full flex flex-row mt-[26px] space-x-4'>
                 <TabsTrigger
-                    onClick={() => onSwitchTab(USER_STATUS.ACTIVATED)}
-                    value={USER_STATUS.ACTIVATED}
+                    onClick={() => onSwitchTab(USER_ASSIGN_STATUS_TABS.assigned)}
+                    value={USER_ASSIGN_STATUS_TABS.assigned}
                     className={
                         'w-1/2 h-[54px] text-lg text-bms-gray-dark data-[state=active]:text-bms-link data-[state=active]: border-bms-link data-[state=active]:font-bold data-[state=active]:border-b-4 data-[state=active]:bg-white sm:truncate ...'
                     }
                 >
-                    {`Active (${userCount?.active ?? 0})`}
+                    {`Assigned (${assignedCount?.active ?? 0})`}
                 </TabsTrigger>
                 <TabsTrigger
-                    onClick={() => onSwitchTab(USER_STATUS.INACTIVE)}
-                    value={USER_STATUS.INACTIVE}
+                    onClick={() => onSwitchTab(USER_ASSIGN_STATUS_TABS.unassigned)}
+                    value={USER_ASSIGN_STATUS_TABS.unassigned}
                     className={
                         'w-1/2 h-[54px] text-lg text-bms-gray-dark data-[state=active]:text-bms-link data-[state=active]: border-bms-link data-[state=active]:font-bold data-[state=active]:border-b-4 data-[state=active]:bg-white sm:truncate ...'
                     }
                 >
-                    {`Archived (${userCount?.inactive ?? 0})`}
+                    {`Unassigned (${unassignedCount?.active ?? 0})`}
+                </TabsTrigger>
+                <TabsTrigger
+                    onClick={() => onSwitchTab(USER_ASSIGN_STATUS_TABS.archive)}
+                    value={USER_ASSIGN_STATUS_TABS.archive}
+                    className={
+                        'w-1/2 h-[54px] text-lg text-bms-gray-dark data-[state=active]:text-bms-link data-[state=active]: border-bms-link data-[state=active]:font-bold data-[state=active]:border-b-4 data-[state=active]:bg-white sm:truncate ...'
+                    }
+                >
+                    {`Archived (${archivedCount?.inactive ?? 0})`}
                 </TabsTrigger>
             </TabsList>
         </Tabs>
