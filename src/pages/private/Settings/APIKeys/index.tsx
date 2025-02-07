96432ee62c11ff2a078fc27f2e3a87651748ef00
Cyrus Belanio <cyrus.belanio@hoolisoftware.com>
2025-02-08 01:08:23 +0800

    BMS-283 apply settings

--- src/pages/private/Settings/APIKeys/index.tsx
+++ src/pages/private/Settings/APIKeys/index.tsx
@@ -4,11 +4,13 @@ import { SMSForm } from './SMSForm'
 import { MapsForm } from './MapsForm'
 import { AndroidForm } from './AndroidForm'
 import { SnapshotRetentionForm } from './SnapshotRetentionForm'
+import { FPTresholdForm } from './FPThresholdForm'
+// import { StragerDetectionForm } from './StrangerDetectionForm'
 
 export const APIKeys = () => {
     return (
         <div className='content'>
-            <BreadCrumbs origin='Settings' title='API Keys' />
+            <BreadCrumbs origin='Settings' title='Device and Admin Settings' />
             <div className='flex flex-col gap-5 w-[50%]'>
                 <div className='flex flex-row gap-5'>
                     <EmailForm />
@@ -20,7 +22,11 @@ export const APIKeys = () => {
                 </div>
                 <div className='flex flex-row gap-5'>
                     <SnapshotRetentionForm />
+                    <FPTresholdForm />
                 </div>
+                {/* <div className='flex flex-row gap-5'>
+                    <StragerDetectionForm />
+                </div> */}
             </div>
         </div>
     )
