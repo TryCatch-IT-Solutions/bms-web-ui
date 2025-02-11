b8c23ebb9075488c67e3935af4bab509dc84b29e
Cyrus Belanio <cyrus.belanio@hoolisoftware.com>
2025-02-11 21:47:57 +0800

    BMS-347 add dynamic logo on settings

--- src/api/settings/schema.ts
+++ src/api/settings/schema.ts
@@ -13,6 +13,12 @@ export const APIKeySchema = z.object({
 })
 
 export const createAPIKeySchema = APIKeySchema.omit({ id: true })
+export const uploadLogoSchema = APIKeySchema.omit({ id: true, value: true }).merge(
+    z.object({
+        value: z.instanceof(File),
+    }),
+)
 
 export type APIKeyType = z.infer<typeof APIKeySchema>
 export type CreateAPIKeyType = z.infer<typeof createAPIKeySchema>
+export type UploadLogoType = z.infer<typeof uploadLogoSchema>
