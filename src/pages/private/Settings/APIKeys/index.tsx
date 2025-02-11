959f1446f09bc838bba23b50b442df1fc8f5e417
Cyrus Belanio <cyrus.belanio@hoolisoftware.com>
2025-02-11 15:27:14 +0800

    BMS-347 apply stranger detection in settings

--- src/pages/private/Settings/APIKeys/index.tsx
+++ src/pages/private/Settings/APIKeys/index.tsx
@@ -24,7 +24,7 @@ export const APIKeys = () => {
                     <SnapshotRetentionForm />
                     <FPTresholdForm />
                 </div>
-                <div className='flex flex-row xs:flex-col sm:flex-col gap-5'>
+                <div className='xs:flex-col sm:flex-col xs:gap-5 sm:gap-5'>
                     <StragerDetectionForm />
                 </div>
             </div>
